08cc351e148fa1246f25db7e25ef96906a7f30f9
Fluva74 <85516975+Fluva74@users.noreply.github.com>
2022-04-26 18:38:04 -0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,7 @@ import Hidden from "@material-ui/core/Hidden";
 const useStyles = makeStyles((theme) => ({
   mainTitle: {
     color: "#ffbf00",
-},
+  },
   cardMedia: {
     width: 160,
     flex: 2,
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
   card: {
     display: "flex",
     justifyContent: "center",
-        height: 300,
+    height: 300,
   },
   cardContent: {
     alignSelf: "center",
@@ -36,8 +36,8 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: "600",
   },
   icon: {
-    color:  "#ffbf00",
-},
+    color: "#ffbf00",
+  },
   links: {
     display: "flex",
     marginRight: "auto",
@@ -74,7 +74,7 @@ function Project({ title, description, imageUrl, tags, links, notice, }) {
   return (
     <Grid item>
       <div>
-      <Card className={styles.card}>
+        <Card className={styles.card}>
           <CardContent className={styles.cardContent}>
             <Typography className={styles.cardTitle} paragraph>
               {title}
@@ -87,28 +87,28 @@ function Project({ title, description, imageUrl, tags, links, notice, }) {
             </Hidden>
           </CardContent>
           <Hidden xsDown>
-          <CardMedia className={styles.cardMedia} image={imageUrl}></CardMedia>
-        </Hidden>
-      </Card>
-</div>
-          <CardActions>
-            <div className={styles.links}>
+            <CardMedia className={styles.cardMedia} image={imageUrl}></CardMedia>
+          </Hidden>
+        </Card>
+      </div>
+      <CardActions>
+        <div className={styles.links}>
           {links.map((linkItem) => (
             <IconButton href={linkItem.href} key={linkItem.href}>
               <linkItem.icon className={styles.icon} />
             </IconButton>
           ))}
-          </div>
-            <div>
+        </div>
+        <div>
           <Typography className={styles.notice} variant="h7" paragraph>
-              {notice}
-              </Typography>
-            </div>
-            <Hidden smDown>
-              <TagsContainer tags={tags} />
-            </Hidden>
-          </CardActions>
-        </Grid>
+            {notice}
+          </Typography>
+        </div>
+        <Hidden smDown>
+          <TagsContainer tags={tags} />
+        </Hidden>
+      </CardActions>
+    </Grid>
   );
 }
 
@@ -130,76 +130,77 @@ export default function Projects() {
 }
 
 const projectsData = [
-    {
-      title: "Big Lebowski Quote Generator",
-      description: "Random quotes from the movie 'The Big Lebowski'.",
-      imageUrl: "https://static3.srcdn.com/wordpress/wp-content/uploads/2021/06/the-big-lebowski-the-stranger-real-imaginary-narrator.jpg?q=50&fit=crop&w=960&h=500&dpr=1.5",
-      imageAlt: "Project 1 Image.",
-      tags: ["React", "Node.js", "MongoDB", "Rest API"],
-      notice: "This app was deployed on Heroku. Please allow up to 30 seconds on initial load time.",
-      links: [
-          {
-              icon: GitHubIcon,
-              href: "https://github.com/Fluva74/api_project",
-          },
-          { 
-              icon: OpenInNewIcon,
-              href: "https://lebowski-quote-generator.herokuapp.com",
-          },
+  {
+    title: "Big Lebowski Quote Generator",
+    description: "Random quotes from the movie 'The Big Lebowski'.",
+    imageUrl: "https://static3.srcdn.com/wordpress/wp-content/uploads/2021/06/the-big-lebowski-the-stranger-real-imaginary-narrator.jpg?q=50&fit=crop&w=960&h=500&dpr=1.5",
+    imageAlt: "Project 1 Image.",
+    tags: ["React", "Node.js", "MongoDB", "Rest API"],
+    notice: "This app was deployed on Heroku. Please allow up to 30 seconds on initial load time.",
+    links: [
+      {
+        icon: GitHubIcon,
+        href: "https://github.com/Fluva74/api_project",
+      },
+      {
+        icon: OpenInNewIcon,
+        href: "https://lebowski-quote-generator.herokuapp.com",
+        target: blank,
+      },
     ],
-      
-  },
-    {
-      title: "Margarita Mockup",
-      description: "Mockup site about margaritas",
-      imageUrl: "https://i.ibb.co/R7S3z2C/Screenshot-2022-02-18-at-12-48-00-Margaritas.png",
-      imageAlt: "Project 2 Image.",
-      tags: ["React"],
-      links: [
-          {
-              icon: GitHubIcon,
-              href: "https://github.com/Fluva74/building1",
-          },
-          { 
-              icon: OpenInNewIcon,
-              href: "https://margaritatales.netlify.app/",
-          },
-    ],
-      
+
   },
   {
-      title: "dinner?",
-      description: "A recipe app with search option.",
-      imageUrl: "https://i.ibb.co/S0wWdH8/Screenshot-2022-01-27-at-15-36-35-React-App.png",
-      imageAlt: "Project 3 Image.",
+    title: "Margarita Mockup",
+    description: "Mockup site about margaritas",
+    imageUrl: "https://i.ibb.co/R7S3z2C/Screenshot-2022-02-18-at-12-48-00-Margaritas.png",
+    imageAlt: "Project 2 Image.",
+    tags: ["React"],
+    links: [
+      {
+        icon: GitHubIcon,
+        href: "https://github.com/Fluva74/building1",
+      },
+      {
+        icon: OpenInNewIcon,
+        href: "https://margaritatales.netlify.app/",
+      },
+    ],
+
+  },
+  {
+    title: "dinner?",
+    description: "A recipe app with search option.",
+    imageUrl: "https://i.ibb.co/S0wWdH8/Screenshot-2022-01-27-at-15-36-35-React-App.png",
+    imageAlt: "Project 3 Image.",
     tags: ["React.js", "Rest API", "Node.js"],
     notice: "This app was deployed on Heroku. Please allow up to 30 seconds on initial load time.",
-      links: [
-          {
-              icon: GitHubIcon,
-              href: "https://github.com/Fluva74/react-recipe",
-          },
-          { 
-              icon: OpenInNewIcon,
-              href: "https://dinner-recipe-app.herokuapp.com/",
-          },
-      ],
+    links: [
+      {
+        icon: GitHubIcon,
+        href: "https://github.com/Fluva74/react-recipe",
+      },
+      {
+        icon: OpenInNewIcon,
+        href: "https://dinner-recipe-app.herokuapp.com/",
+      },
+    ],
   },
   {
-      title: "Personal Portfolio",
-      description: "A portfolio created with Gatsby and Material UI.",
-      imageUrl: "https://i.ibb.co/qj191Bc/Screenshot-2022-01-28-at-11-03-02-https-mikefinneganportfolio-gatsbyjs-io.png",
-      imageAlt: "Project 4 Image.",
-      tags: ["Gatsby", "Material-UI", "React.js"],
-      links: [
-          {
-              icon: GitHubIcon,
-              href: "https://github.com/Fluva74/mike-finnegan-portfolio",
-          },
-          { 
-              icon: OpenInNewIcon,
-              href: "https://mikefinneganportfolio.gatsbyjs.io/",
-          },
-      ],
+    title: "Personal Portfolio",
+    description: "A portfolio created with Gatsby and Material UI.",
+    imageUrl: "https://i.ibb.co/qj191Bc/Screenshot-2022-01-28-at-11-03-02-https-mikefinneganportfolio-gatsbyjs-io.png",
+    imageAlt: "Project 4 Image.",
+    tags: ["Gatsby", "Material-UI", "React.js"],
+    links: [
+      {
+        icon: GitHubIcon,
+        href: "https://github.com/Fluva74/mike-finnegan-portfolio",
+      },
+      {
+        icon: OpenInNewIcon,
+        href: "https://mikefinneganportfolio.gatsbyjs.io/",
+      },
+    ],
   },
 ];
